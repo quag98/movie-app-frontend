507f757efc5b3b1eddb21aff8a39d60357cc007a
David Quaggin-Smith <quagginsmith@gmail.com>
2021-04-05 10:38:16 -0500

    adds homework

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Movie List</h1>
+    <div>
+      <div v-for="movie in movies" v-bind:key="movie.id">
+        <h3>{{ movie.title }}</h3>
+        <button v-on:click="showMovie(movie)">Show Info</button>
+    </div>
+     <div>
+      <button v-on:click="createMovie()">Create movie</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      movies: [],
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: "",
+    };
+  },
+created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/api/movies").then(response => {
+        this.movies = response.data;
+        console.log(this.movies);
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+      };
+      axios
+        .post("api/movies", params)
+        .then(response => {
+          console.log(response.data);
+          this.movies.push(response.data);
+        })
+        .catch(error => console.log(error.response));
+    },
+    showMove: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+    },
+    updateMove: function (movie) {
+      var params = {
+        title: movie.title,
+        year: movie.year,
+        plot: movie.plot
+      };
+      axios.patch("/api/movies/" + movie.id, params).then(response => {
+        console.log("OK Push", response.data);
+      });
+    },
+        deleteMovies: function (movie) {
+      axios.delete("/api/movie/" + movie.id).then(response => {
+        console.log("movie has been deleted", response);
+        this.$router.push("/movies");
+      });
+    },
   },
 };
 </script>
